adcd9aa758b2ed58733a59e2f44c89a7ea5042b4
TheVizWiz <vibhusiyer@gmail.com>
2024-01-28 22:32:32 -0500

    cleanup files

--- firmware/keymap.c
+++ firmware/keymap.c
@@ -24,19 +24,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_5x6(
         KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_F6  , KC_F7  , KC_F8  , KC_F9 , KC_F10 , KC_PSCR,
         KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , KC_MINS,
-        KC_LSFT, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
+        KC_GRV , KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L  , KC_SCLN, KC_QUOT,
         KC_LCTL, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_BSLS,
                           KC_LBRC, KC_RBRC,                                                       KC_MINS, KC_EQL,
                                             MO_L1  , KC_LCTL,                   KC_SPC , MO_L2  ,
-                                            KC_LGUI, KC_LALT,                   KC_RSFT, KC_BSPC,
-                                            KC_BSPC, KC_LSFT,                   KC_RCTL, KC_RALT
+                                            KC_LALT, KC_LALT,                   KC_RSFT, KC_BSPC,
+                                            KC_BSPC, KC_LGUI,                   KC_RCTL, KC_RALT
         ),
 
     [_L1] = LAYOUT_5x6(
           QK_BOOT, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                 KC_BSLS, KC_SLSH, KC_PAST, KC_MINS, KC_PLUS , KC_F11  ,
           TOG_GAM, _______, _______, KC_UP  , _______, KC_LBRC,                 KC_RBRC, KC_7   , KC_8   , KC_9   , KC_NUM  , KC_MUTE ,
-          RGB_MOD, KC_LEFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_LPRN,                 KC_RPRN, KC_4   , KC_5   , KC_6   , _______ , KC_VOLU ,
-          RGB_VAI, RGB_VAD, _______, _______, _______, _______,                 _______, KC_1   , KC_2   , KC_3   , _______ , KC_VOLD ,
+          RGB_MOD, KC_LEFT, KC_LEFT, KC_DOWN, KC_RGHT, KC_LPRN,                 KC_RPRN, KC_4   , KC_5   , KC_6   , KC_DOT  , KC_VOLU ,
+          RGB_VAI, RGB_VAD, _______, KC_SPC , _______, _______,                 KC_GRV , KC_1   , KC_2   , KC_3   , KC_COMM , KC_VOLD ,
                             _______, RGB_TOG,                                                     _______, _______,
                                               _______, _______,                 KC_PENT, KC_0   ,
                                               _______, _______,                 _______, KC_DEL ,
@@ -45,9 +45,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_L2] = LAYOUT_5x6(
         QK_BOOT, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                 KC_F6  , KC_MPRV, KC_MPLY, KC_MNXT, KC_F10 , QK_BOOT,
-        TOG_GAM, _______, _______, KC_UP  , _______, RGB_HUI,                 KC_RBRC, _______, KC_UP  , KC_INS , KC_SCRL, KC_MUTE,
-        RGB_MOD, KC_LEFT, KC_LEFT, KC_DOWN, KC_RGHT, RGB_SAI,                 KC_RPRN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_VOLU,
-        _______, _______, _______, _______, _______, RGB_VAI,                 _______, _______, _______, _______, _______, KC_VOLD,
+        TOG_GAM, _______, _______, KC_UP  , _______, _______,                 KC_RBRC, _______, KC_UP  , KC_INS , KC_SCRL, KC_MUTE,
+        _______, KC_LEFT, KC_LEFT, KC_DOWN, KC_RGHT, _______,                 KC_RPRN, KC_LEFT, KC_DOWN, KC_RGHT, _______, KC_VOLU,
+        _______, _______, _______, _______, _______, _______,                 _______, _______, _______, _______, _______, KC_VOLD,
                           _______, RGB_TOG,                                                     KC_EQL , _______,
                                             _______, _______,                 KC_TAB ,_______,
                                             _______, _______,                 _______,_______,  
